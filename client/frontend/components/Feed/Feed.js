5f855da8a8dd8aaa0b62adf71c46bcf5e8bae2b2
unknown <gomesluisfelipe05@gmail.com>
2022-11-01 14:49:41 -0300

    feed

--- client/frontend/components/Feed/Feed.js
+++ client/frontend/components/Feed/Feed.js
@@ -1,17 +1,120 @@
 import React from 'react'
-import { View, Text, Button } from 'react-native'
+import { View, Text, Button, StatusBar, TouchableOpacity, FlatList, Image } from 'react-native'
 import { auth } from '../../firebase/config'
 import { signOut } from 'firebase/auth'
+import { styles } from './styles'
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+// https://oblador.github.io/react-native-vector-icons/
+// hand-peace-variant
+// comment
+// share
 
 function Feed() {
+  const img = 'https://i0.wp.com/jornal.usp.br/wp-content/uploads/2020/10/20201027_00_corinthians_torcida.jpg?w=1200&ssl=1'
+  const posts = [
+    {name:'Luis Felipe', profileImg:img, created_at:'hh/mm. quinta 28 de out', caption:'Futebol é realmente uma paixão de outras vidas, Mais uma vitoria pra conta no jogo hoje', comments: 3, likes: 10, postImg: img},
+    {name:'Tiago Silva', profileImg:'', created_at:'hh/mm. quinta 28 de out', caption:'Futebol é realmente uma paixão de outras vidas, Mais uma vitoria pra conta no jogo hoje', comments: 3, likes: 10, postImg: img},
+    {name:'Camila Piovesan', profileImg:img, created_at:'hh/mm. quinta 28 de out', caption:'Futebol é realmente uma paixão de outras vidas, Mais uma vitoria pra conta no jogo hoje', comments: 3, likes: 10, postImg: img},
+    {name:'Osmar Guideli', profileImg:img, created_at:'hh/mm. quinta 28 de out', caption:'Futebol é realmente uma paixão de outras vidas, Mais uma vitoria pra conta no jogo hoje', comments: 3, likes: 10, postImg: img},
+    {name:'Osmar Guideli', profileImg:img, created_at:'hh/mm. quinta 28 de out', caption:'Futebol é realmente uma paixão de outras vidas, Mais uma vitoria pra conta no jogo hoje', comments: 3, likes: 10, postImg: img},
+  ]
 
   return (
-    <View>
+    <View style={{flex: 1}}>
+      <View style={styles.contentTop}>
+        <View style={styles.header}>
+          <View style={styles.content}>
+            <Text style={styles.logo}>ii</Text>
+            <View style={styles.containerProfile}></View>
+          </View>
+        </View>
+        
+      </View>
+        <FlatList 
+          ListHeaderComponent={
+          <View style={styles.containerOptions}>
+            <View style={styles.options}>
+              <TouchableOpacity>
+                <MaterialCommunityIcons name="calendar" color={'#fff'} size={26} />
+              </TouchableOpacity>
+              <TouchableOpacity>
+                <MaterialCommunityIcons name="camera" color={'#fff'} size={26} />
+              </TouchableOpacity>
+            </View>
+          </View>}
+          numColumns={1}
+          horizontal={false}
+          data={posts}
+          renderItem={({item}) => (
+            <View style={styles.containerPost}>
+              <View style={styles.postHeader}>
+
+                <View style={styles.profileImg}>
+                  <Image 
+                    style={styles.profileImg}
+                    source={item.profileImg ? {uri: item.profileImg} : {uri: "https://www.mnhockeyhub.com/app_images/no_photo_neutral.svg?1657912251"}}
+                  />
+                </View>
+
+                <View style={styles.postInfo}>
+                  <Text style={styles.username}>{item.name}</Text>
+                  <Text style={styles.created_at}>{item.created_at}</Text>
+                </View>
+                
+              </View>
+
+              {item.postImg && (
+                <View style={styles.containerPostImg}>
+                  <Image
+                    style={styles.postImg}
+                    source={{uri: item.postImg}}
+                  />
+                </View>
+              )}
+
+              {item.caption && (
+                <View style={styles.captionContainer}>
+                  <Text style={styles.caption}>{item.caption}</Text>
+                </View>
+              )}
+
+              <View style={styles.containerBottom}>
+                <View style={styles.postInteractions}>
+                  <View style={styles.icon}>
+                    <View style={styles.InteractionNumber}>
+                      <Text style={styles.number}>13</Text>
+                    </View>
+                    <TouchableOpacity>
+                      <MaterialCommunityIcons name="hand-peace-variant" color={'#fff'} size={30} />
+                    </TouchableOpacity>
+                  </View>
+                  <View style={styles.icon}>
+                    <View style={styles.InteractionNumber}>
+                      <Text style={styles.number}>13</Text>
+                    </View>
+                    <TouchableOpacity>
+                      <MaterialCommunityIcons name="comment" color={'#fff'} size={30} />
+                    </TouchableOpacity>
+                  </View>
+                  <View style={styles.icon}>
+                    <View style={styles.InteractionNumber}>
+                      <Text style={styles.number}>13</Text>
+                    </View>
+                    <TouchableOpacity>
+                      <MaterialCommunityIcons name="share" color={'#fff'} size={30} />
+                    </TouchableOpacity>
+                  </View>
+                </View>
+              </View>
+            </View>
+          )}
+        />
         <Text>Feed</Text>
-        <Button 
+        <Button
             title='Sair'
             onPress={() => signOut(auth)}
         />
+        <StatusBar />
     </View>
   )
 }
