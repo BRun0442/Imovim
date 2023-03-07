4ce394fabc6ade99e09bc9c4f36edb87ffb2a5cf
luisfelipe2104 <100540998+luisfelipe2104@users.noreply.github.com>
2023-03-07 11:20:48 -0300

    consuming the route of editing profile

--- components/Pages/EditarPerfil/EditarPerfil.js
+++ components/Pages/EditarPerfil/EditarPerfil.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useContext } from "react";
-import { View, Text, StatusBar, TouchableOpacity, Image } from "react-native";
+import { View, Text, StatusBar, TouchableOpacity, Image, Alert } from "react-native";
 import { ScrollView, TextInput } from "react-native-gesture-handler";
 import Header from "../../Header/Header";
 import { styles } from './style.js'
@@ -8,28 +8,30 @@ import * as ImagePicker from "expo-image-picker";
 import * as MediaLibrary from "expo-media-library";
 import axios from "axios";
 import { AuthContext } from "../../../contexts/auth";
+import { updateProfile } from "../../../services/profile";
 
-function EditarPerfil() {
+function EditarPerfil({ navigation }) {
     const { id } = useContext(AuthContext)
-    const handleSubmit = async (imageUrl) => {
-        const data = {
-            user_id: id,
-            image: imageUrl
-        }
-        await axios.post(`https://imovim-api.cyclic.app/profile/update-profile-img`, data)
-        .then((res) => {
-            console.log(res)
+    const [image, setImage] = useState(null);
+    const [backgroundImage, setBackgroundImage] = useState(null);
+    const [localization, setLocalization] = useState('');
+    const [loaded, setLoaded] = useState(false)
+
+    const handleSubmit = async () => {
+        await updateProfile(image, id, backgroundImage, localization)
+        .then(() => {
+            Alert.alert('Perfil atualizado!')
+            navigation.navigate('Perfil')
         })
     }
     
     const [galleryPermission, setGalleryPermission] = useState(null);
-    const [image, setImage] = useState(null);
     const galeryPermisionFunction = async () => {
         const galleryPermissions = await MediaLibrary.requestPermissionsAsync();
         setGalleryPermission(galleryPermissions.granted);
       };
           
-      const pickImage = async () => {
+      const pickImage = async (set) => {
         // No permissions request is necessary for launching the image library
         let result = await ImagePicker.launchImageLibraryAsync({
           mediaTypes: ImagePicker.MediaTypeOptions.Images,
@@ -39,16 +41,15 @@ function EditarPerfil() {
         });
         
         if (!result.cancelled) {
-          setImage(result.uri);
-          handleSubmit(result.uri)
+          set(result.uri)
           console.log(result.uri)
           // UploadImage(result.uri, setImageUrl)  // PRECISAMOS ARRUMAR ESSA GAMBIARRA!!!
         }
       };
-    const main = () => {
+    const main = (set) => {
         if(galleryPermission == true)
             {
-              pickImage();
+              pickImage(set);
             }else{
               galeryPermisionFunction();
             }
@@ -56,14 +57,36 @@ function EditarPerfil() {
 
     useEffect(() => {
         galeryPermisionFunction();
+        const getProfileData = async () => {
+            await axios.get(`https://imovim-api.cyclic.app/profile/get-editingProfile-data/${id}`)
+            .then((res) => {
+                console.log(res.data)
+                res.data.forEach(element => {
+                    setImage(element.profileImage)
+                    setBackgroundImage(element.profileBackground)
+                    setLocalization(element.localization)
+                    setLoaded(true)
+                });
+            })
+        }
+        getProfileData()
     })
+    if(!loaded) {
+        return(
+            <View>
+                <Text>Loading...</Text>
+            </View>
+        )
+    }
     return (
         <ScrollView style={styles.container}>
             <Header />
 
             <View style={styles.titleContainer}>
                 <Text style={styles.title}>Editar Perfil</Text>
-                <Entypo name="save" size={35} color="#F8670E" />
+                <TouchableOpacity onPress={() => handleSubmit()}>
+                    <Entypo name="save" size={35} color="#F8670E" />
+                </TouchableOpacity>
             </View>
 
             <View style={styles.editProfileContainer}>
@@ -72,7 +95,7 @@ function EditarPerfil() {
                     <Text style={styles.editProfileTitle}>Foto do Perfil</Text>
                 </View>
 
-                <TouchableOpacity onPress={() => main()} style={styles.editProfileIconContainerPhoto}>
+                <TouchableOpacity onPress={() => main(setImage)} style={styles.editProfileIconContainerPhoto}>
                     {!image && (
                     <View style={styles.editPhoto}>
                         <Entypo name="camera" size={22} color="#ADADAD" />
@@ -103,11 +126,25 @@ function EditarPerfil() {
                     <Text style={styles.editProfileTitle}>Foto de Capa</Text>
                 </View>
 
-                <View style={styles.editProfileIconContainerBanner}>
-                    <View style={styles.editBanner}>
-                        <Entypo name="camera" size={22} color="#ADADAD" />
-                    </View>
-                </View>
+                <TouchableOpacity onPress={() => main(setBackgroundImage)} style={styles.editProfileIconContainerBanner}>
+                    { !backgroundImage && (
+                        <View style={styles.editBanner}>
+                            <Entypo name="camera" size={22} color="#ADADAD" />
+                        </View>
+                    ) }
+                    { backgroundImage && (
+                        <Image 
+                        style =
+                        {
+                          {
+                              width: '95%',
+                              height: 150,
+                          }
+                        }
+                        source={{uri: backgroundImage}} 
+                        /> 
+                    ) }
+                </TouchableOpacity>
 
                 <View style={styles.lineContainer}>
                     <View style={styles.line}></View>
@@ -118,7 +155,7 @@ function EditarPerfil() {
                 </View>
 
                 <View style={styles.editProfileIconContainerInput}>
-                    <TextInput style={styles.input} />
+                    <TextInput value={localization} onChangeText={(text) => {setLocalization(text)}} style={styles.input} />
                 </View>
 
             </View>
---
+++ services/profile.js
@@ -0,0 +1,101 @@
+import { firebaseConfig, storage } from "../firebase/config.js";
+import { uploadBytes, getDownloadURL, ref, getStorage } from "firebase/storage";
+import { initializeApp } from "firebase/app";
+import axios from "axios";
+
+initializeApp(firebaseConfig);
+
+export const sendProfileDataToApi = async (user_id, image, background, localization) => {
+    const data = {
+        user_id: user_id, 
+        image: image, 
+        background: background, 
+        localization: localization
+    }
+    await axios.post(`https://imovim-api.cyclic.app/profile/update-profile`, data)
+    .then((res) => {
+        console.log(res)
+    })
+}
+
+export const updateBackgroundInApi = async (user_id, background) => {
+    const data = {
+        user_id: user_id,
+        background: background
+    }
+    await axios.post(`https://imovim-api.cyclic.app/profile/update-profile-background`, data)
+    .then((res) => {
+        console.log(res)
+    })
+}
+
+export const updateBackground = async (user_id, background) => {
+  if(background)
+  {
+    let imageRef;
+    const storageUrl = `backgroundImage/${Date.now()}/${Math.random().toString()}`;
+    const response = await fetch(background);
+    const bytes = await response.blob(); // creates a blob from the uri and pass to firestore
+    imageRef = ref(storage, storageUrl);
+
+    uploadBytes(imageRef, bytes)
+    .then(() => {
+      getDownloadURL(imageRef)
+        .then(async (url) => {
+
+        /// code goes here
+          await updateBackgroundInApi(user_id, url)  
+          .then(res => console.log(res))
+          console.log('url:', url)
+          url = ''
+          return url
+        })
+        .catch((err) => {
+          console.log(err.message);
+        });
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+  }else{
+    imageRef = null;
+    updateBackgroundInApi(user_id, '')  
+    return;
+  }
+}
+
+export const updateProfile = async (image, user_id, background, localization) => {
+    if(image)
+  {
+    let imageRef;
+    const storageUrl = `profileImage/${Date.now()}/${Math.random().toString()}`;
+    const response = await fetch(image);
+    const bytes = await response.blob(); // creates a blob from the uri and pass to firestore
+    imageRef = ref(storage, storageUrl);
+
+    uploadBytes(imageRef, bytes)
+    .then(() => {
+      getDownloadURL(imageRef)
+        .then(async (url) => {
+          await sendProfileDataToApi(user_id, url, background, localization)  
+          .then(res => console.log(res))
+          console.log('url:', url)
+          url = ''
+          return url
+        })
+        .catch((err) => {
+          console.log(err.message);
+        });
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+  }else{
+    imageRef = null;
+    sendProfileDataToApi(user_id, '', background, localization)  
+    return;
+  }
+  if (background) {
+    await updateBackground(user_id, background)
+  }
+}
