f6b08f54f0b4fa432d0c20fc59f52435d3c04da0
luisfelipe2104 <100540998+luisfelipe2104@users.noreply.github.com>
2023-03-07 12:08:24 -0300

    showing profile image in the profile screen

--- components/Pages/PerfilVisãoInterna/Perfil.js
+++ components/Pages/PerfilVisãoInterna/Perfil.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { View, Text, StatusBar } from 'react-native';
+import React, { useState, useContext, useEffect } from 'react';
+import { View, Text, StatusBar, Image } from 'react-native';
 import { styles } from './style'
 import TopBar from '../../TopBar/TopBar';
 import { TouchableOpacity, ScrollView } from 'react-native-gesture-handler';
@@ -13,20 +13,13 @@ import { FontAwesome5 } from '@expo/vector-icons';
 import { AntDesign } from '@expo/vector-icons';
 {/* <FontAwesome5 name="user-check" size={24} color="black" /> */ }
 // icone para quando a pessoa clicar em adicionar não sei por sapoha Ainda...
+import { AuthContext } from '../../../contexts/auth';
 
 export default function PerfilVisãoInterna({ navigation }, props) {
-
+    const { profilePicture } = useContext(AuthContext)
     const [visible, setVisible] = useState(false)
 
     console.log(visible);
-
-    // if (visible === false) {
-    //     return (
-
-    //     )
-    // } else {
-
-    // }
 
     return (
         <ScrollView style={styles.container}>
@@ -38,7 +31,17 @@ export default function PerfilVisãoInterna({ navigation }, props) {
 
                 <View style={styles.icons}>
                     <View style={styles.iconCam}>
-                        <Entypo name="camera" size={22} color="#FFF" />
+                    <Image 
+                       style =
+                       {
+                         {
+                             width: '100%',
+                             height: '100%',
+                             borderRadius: 100
+                         }
+                       }
+                       source={{uri: profilePicture || 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png'}} 
+                       /> 
                     </View>
                     <TouchableOpacity style={styles.iconPencil}
                         onPress={() => { navigation.navigate('Editar Perfil') }}
