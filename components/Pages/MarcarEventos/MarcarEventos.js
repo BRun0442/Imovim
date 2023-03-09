9f9032711fec65d619b7b2cc0b5e54600592970e
luisfelipe2104 <100540998+luisfelipe2104@users.noreply.github.com>
2023-03-09 17:36:07 -0300

    creating events and saving them in the database

--- components/Pages/MarcarEventos/MarcarEventos.js
+++ components/Pages/MarcarEventos/MarcarEventos.js
@@ -1,14 +1,72 @@
-import React from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { View, Text, TextInput, StatusBar } from 'react-native';
 import { TouchableOpacity, ScrollView } from 'react-native-gesture-handler';
 import { styles } from './style'
 import Header from '../../Header/Header';
 import ProfileImage from '../../ProfileImage/ProfileImage';
-
+import { AuthContext } from '../../../contexts/auth';
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 import { Entypo } from '@expo/vector-icons';
+import { createEvent } from '../../../services/events';
+import * as ImagePicker from "expo-image-picker";
+import * as MediaLibrary from "expo-media-library";
+import { Image } from 'react-native';
 
 export default function MarcarEventos(props) {
+    const { id } = useContext(AuthContext);
+    const [name, setName] = useState()
+    const [localization, setLocalization] = useState()
+    const [description, setDescription] = useState()
+    const [image, setImage] = useState()
+    const [day, setDay] = useState()
+    const [month, setMonth] = useState()
+    const [year, setYear] = useState()
+    const [hour, setHour] = useState()
+    const [minute, setMinute] = useState()
+    const [galleryPermission, setGalleryPermission] = useState(null);
+
+    useEffect(() => {
+        // CameraPermisionFunction();
+        // setImage(null);
+        galeryPermisionFunction();
+      }, []);
+    
+      const galeryPermisionFunction = async () => {
+        const galleryPermissions = await MediaLibrary.requestPermissionsAsync();
+        setGalleryPermission(galleryPermissions.granted);
+      };
+      
+      
+      const pickImage = async () => {
+        // No permissions request is necessary for launching the image library
+        let result = await ImagePicker.launchImageLibraryAsync({
+          mediaTypes: ImagePicker.MediaTypeOptions.Images,
+          allowsEditing: true,
+          //   aspect: [1, 1],
+          //   quality: 1,
+        });
+        
+        if (!result.cancelled) {
+          setImage(result.uri);
+          // UploadImage(result.uri, setImageUrl)  // PRECISAMOS ARRUMAR ESSA GAMBIARRA!!!
+        }
+      };
+    
+      const main = async () => {
+        if(galleryPermission == true)
+            {
+              pickImage();
+            }else{
+              galeryPermisionFunction();
+            }
+      }
+    const handleSubmit = async () => {
+        console.log(id, name, localization, description, day, month, year, hour, minute)
+        const eventDate = `${year}/${month}/${day}`
+        const eventHour = `${hour}:${minute}`
+        await createEvent(id, name, eventDate, eventHour, localization, description, image, setImage)
+    }
+
     return (
         <View style={styles.container}>
 
@@ -37,6 +95,7 @@ export default function MarcarEventos(props) {
                     <Text style={styles.formText}>Nome</Text>
                     <View style={styles.nameInputs}>
                         <TextInput style={styles.inputType1}
+                            onChangeText={(text) => setName(text)}
                             keyboardType='default'
                         />
                     </View>
@@ -45,16 +104,19 @@ export default function MarcarEventos(props) {
                     <View style={styles.dataInputs}>
 
                         <TextInput style={styles.inputType2}
+                            onChangeText={(text) => setDay(text)}
                             placeholder="Dia"
                             keyboardType='numeric'
                         />
 
                         <TextInput style={styles.inputType2}
+                            onChangeText={(text) => setMonth(text)}
                             placeholder="Mês"
                             keyboardType='numeric'
                         />
 
                         <TextInput style={styles.inputType2}
+                            onChangeText={(text) => setYear(text)}
                             placeholder="Ano"
                             keyboardType='numeric'
                         />
@@ -64,11 +126,13 @@ export default function MarcarEventos(props) {
 
                     <View style={styles.horaInputs}>
                         <TextInput style={styles.inputType2}
+                            onChangeText={(text) => setHour(text)}
                             placeholder="hh"
                             keyboardType='numeric'
                         />
 
                         <TextInput style={styles.inputType2}
+                            onChangeText={(text) => setMinute(text)}
                             placeholder="min"
                             keyboardType='numeric'
                         />
@@ -78,6 +142,7 @@ export default function MarcarEventos(props) {
                     <Text style={styles.formText}>Local</Text>
                     <View style={styles.nameInputs}>
                         <TextInput style={styles.inputType1}
+                            onChangeText={(text) => setLocalization(text)}
                             keyboardType='default'
                         />
                     </View>
@@ -85,6 +150,7 @@ export default function MarcarEventos(props) {
                     <Text style={styles.formText}>Descrição</Text>
                     <View style={styles.nameInputs}>
                         <TextInput style={styles.inputType3}
+                            onChangeText={(text) => setDescription(text)}
                             keyboardType='default'
                         />
                     </View>
@@ -94,19 +160,19 @@ export default function MarcarEventos(props) {
                     <View style={styles.banner}>
                         <TouchableOpacity
 
-                            // onPress={() => main(setBackgroundImage)} 
+                            onPress={() => main()} 
 
                             style={styles.editProfileIconContainerBanner}>
 
-                            {/* {!backgroundImage && ( */}
+                            {!image && (
 
                             <View style={styles.editBanner}>
                                 <Entypo name="camera" size={22} color="#FFF" />
                             </View>
 
-                            {/* )} */}
+                            )}
 
-                            {/* {backgroundImage && (
+                            {image && (
                             <Image
                                 style=
                                 {
@@ -115,13 +181,13 @@ export default function MarcarEventos(props) {
                                         height: 150,
                                     }
                                 }
-                                source={{ uri: backgroundImage }}
+                                source={{ uri: image }}
                             />
-                        )} */}
+                        )}
                         </TouchableOpacity>
                     </View>
 
-                    <TouchableOpacity style={styles.button}>
+                    <TouchableOpacity onPress={() => handleSubmit()} style={styles.button}>
                         <Text style={styles.text}>Criar Evento</Text>
                     </TouchableOpacity>
 
--- services/events.js
+++ services/events.js
@@ -1,6 +1,44 @@
 import api from './api';
+import { firebaseConfig, storage } from "../firebase/config.js";
+import { uploadBytes, getDownloadURL, ref, getStorage } from "firebase/storage";
+import { initializeApp } from "firebase/app";
 
-async function createEvent(user_id, event_name, event_date, event_hour, localization, description, photo)
+export async function createEvent(user_id, event_name, event_date, event_hour, localization, description, image, setImage) {
+  if(image)
+  {
+    let imageRef;
+    const storageUrl = `eventsImage/${Date.now()}/${Math.random().toString()}`;
+    const response = await fetch(image);
+    const bytes = await response.blob(); // creates a blob from the uri and pass to firestore
+    imageRef = ref(storage, storageUrl);
+
+    uploadBytes(imageRef, bytes)
+    .then(() => {
+      getDownloadURL(imageRef)
+        .then(async (url) => {
+          await sendDataToApi(user_id, event_name, event_date, event_hour, localization, description, url)  
+          .then(res => console.log(res))
+          console.log('url:', url)
+          url = ''
+          setImage(null)
+          return url
+        })
+        .catch((err) => {
+          console.log(err.message);
+        });
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+  }else{
+    imageRef = null;
+    sendDataToApi(user_id, event_name, event_date, event_hour, localization, description, image)
+    setImage(null)  
+    return;
+  }
+}
+
+export async function sendDataToApi(user_id, event_name, event_date, event_hour, localization, description, photo)
 {
   try {
     const response = await api.post("/event/create-event", {
@@ -14,6 +52,7 @@ async function createEvent(user_id, event_name, event_date, event_hour, localiza
     })
     
     alert(response.data.msg)
+    console.log(response)
   } catch (error) {
     console.log(error.data.msg)
   }
