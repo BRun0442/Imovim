abcac280c655c694c0d286c6b357074dd74807d1
Tiaguh <tiagosingup@gmail.com>
2023-03-13 17:30:40 -0300

    fixing bugs when rendering profile image

--- components/Pages/CriarPost/CriarPost.js
+++ components/Pages/CriarPost/CriarPost.js
@@ -8,7 +8,8 @@ import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { MaterialIcons } from '@expo/vector-icons';
 import { Entypo } from '@expo/vector-icons';
 import ProfileImage from '../../ProfileImage/ProfileImage';
-
+import { AccountDataContext } from "../../../contexts/accountData";
+import getUserData from "../../../services/user";
 import { AuthContext } from '../../../contexts/auth';
 import { useIsFocused } from "@react-navigation/native";
 import * as ImagePicker from "expo-image-picker";
@@ -17,6 +18,7 @@ import CreatePost from "../../../services/createPost.js";
 
 export default function CriarPost({ navigation }) {
   const { profilePicture } = useContext(AuthContext);
+  const { setAccountData } = useContext(AccountDataContext);
   const { nickname } = useContext(AuthContext);
   const { id } = useContext(AuthContext);
   const [caption, setCaption] = useState('');
@@ -42,7 +44,7 @@ export default function CriarPost({ navigation }) {
     setGalleryPermission(galleryPermissions.granted);
   };
 
-  
+
   const pickImage = async () => {
     // No permissions request is necessary for launching the image library
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -51,7 +53,7 @@ export default function CriarPost({ navigation }) {
       //   aspect: [1, 1],
       //   quality: 1,
     });
-    
+
     if (!result.cancelled) {
       setImage(result.uri);
       // UploadImage(result.uri, setImageUrl)  // PRECISAMOS ARRUMAR ESSA GAMBIARRA!!!
@@ -59,64 +61,64 @@ export default function CriarPost({ navigation }) {
   };
 
   return (
-    <ScrollView style={{backgroundColor: '#fff'}}>
-      <StatusBar/>
+    <ScrollView style={{ backgroundColor: '#fff' }}>
+      <StatusBar />
       <Header navigation={navigation} />
-      
+
       <View style={styles.photoContainer}>
-          <TouchableOpacity style={{alignSelf: 'center'}}>
-              <MaterialCommunityIcons name="camera" color={"#fff"} size={26} />
-          </TouchableOpacity>
+        <TouchableOpacity style={{ alignSelf: 'center' }}>
+          <MaterialCommunityIcons name="camera" color={"#fff"} size={26} />
+        </TouchableOpacity>
       </View>
 
       <View style={styles.postContainer}>
-          <View style={styles.postProfile}>
-              <ProfileImage profileImage={profilePicture} />
-              <View style={styles.profileContainer}>
-                  <Text style={styles.profileName}>{nickname}</Text>
-              </View>
+        <View style={styles.postProfile}>
+          <ProfileImage profileImage={profilePicture} />
+          <View style={styles.profileContainer}>
+            <Text style={styles.profileName}>{nickname}</Text>
           </View>
+        </View>
 
-          <TextInput 
-            style={styles.input} 
-            value={caption}
-            multiline 
-            textAlign='center' 
-            placeholder='Fale sobre uma aventura aqui!' 
-            placeholderTextColor={"#7B7B7B"}
-            maxLength={400}
-            onChangeText={value => setCaption(value)}
-          />
+        <TextInput
+          style={styles.input}
+          value={caption}
+          multiline
+          textAlign='center'
+          placeholder='Fale sobre uma aventura aqui!'
+          placeholderTextColor={"#7B7B7B"}
+          maxLength={400}
+          onChangeText={value => setCaption(value)}
+        />
       </View>
 
       <View style={styles.buttons}>
-          <TouchableOpacity onPress={() => navigation.navigate('Camera')} style={styles.button}>
-              <Entypo name="camera" size={26} color={'#fff'} />
-              <Text style={styles.buttonText}>Câmera</Text>
-          </TouchableOpacity>
+        <TouchableOpacity onPress={() => navigation.navigate('Camera')} style={styles.button}>
+          <Entypo name="camera" size={26} color={'#fff'} />
+          <Text style={styles.buttonText}>Câmera</Text>
+        </TouchableOpacity>
 
-          <TouchableOpacity onPress={() => {
-            if(galleryPermission == true)
-            {
-              pickImage();
-            }else{
-              galeryPermisionFunction();
-            }
-          }} style={styles.button}>
-              <MaterialCommunityIcons name="camera-plus" size={26} color={'#fff'} />
-              <Text style={styles.buttonText}>Adicionar foto/imagem</Text>
-          </TouchableOpacity>
-          
-          <TouchableOpacity style={styles.button}>
-              <MaterialIcons name="place" size={26} color={'#fff'} />
-              <Text style={styles.buttonText}>Localização</Text>
-          </TouchableOpacity>
+        <TouchableOpacity onPress={() => {
+          if (galleryPermission == true) {
+            pickImage();
+          } else {
+            galeryPermisionFunction();
+          }
+        }} style={styles.button}>
+          <MaterialCommunityIcons name="camera-plus" size={26} color={'#fff'} />
+          <Text style={styles.buttonText}>Adicionar foto/imagem</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.button}>
+          <MaterialIcons name="place" size={26} color={'#fff'} />
+          <Text style={styles.buttonText}>Localização</Text>
+        </TouchableOpacity>
       </View>
 
       <Button buttonText='Criar Post' pressFunction={async () => {
-          await CreatePost(image, id, caption, setImage)
-          navigation.navigate('Página Inicial')
-          }}
+        await CreatePost(image, id, caption, setImage)
+        await getUserData(id, setAccountData)
+        navigation.navigate('Página Inicial')
+      }}
       />
     </ScrollView>
   );
--- components/Pages/EditarPerfil/EditarPerfil.js
+++ components/Pages/EditarPerfil/EditarPerfil.js
@@ -9,9 +9,12 @@ import * as MediaLibrary from "expo-media-library";
 import axios from "axios";
 import { AuthContext } from "../../../contexts/auth";
 import { updateProfile } from "../../../services/profile";
+import { AccountDataContext } from "../../../contexts/accountData";
+import getUserData from "../../../services/user";
 
 function EditarPerfil({ navigation }) {
     const { id, setProfilePicture } = useContext(AuthContext)
+    const { setAccountData } = useContext(AccountDataContext);
     const [image, setImage] = useState(null);
     const [backgroundImage, setBackgroundImage] = useState(null);
     const [localization, setLocalization] = useState('');
@@ -19,60 +22,60 @@ function EditarPerfil({ navigation }) {
 
     const handleSubmit = async () => {
         await updateProfile(image, id, backgroundImage, localization, setProfilePicture)
-        .then(() => {
-            Alert.alert('Perfil atualizado!')
-            navigation.navigate('Perfil')
-        })
+            .then(async () => {
+                await getUserData(id, setAccountData)
+                Alert.alert('Perfil atualizado!')
+                navigation.navigate('Perfil')
+            })
     }
-    
+
     const [galleryPermission, setGalleryPermission] = useState(null);
     const galeryPermisionFunction = async () => {
         const galleryPermissions = await MediaLibrary.requestPermissionsAsync();
         setGalleryPermission(galleryPermissions.granted);
-      };
-          
-      const pickImage = async (set) => {
+    };
+
+    const pickImage = async (set) => {
         // No permissions request is necessary for launching the image library
         let result = await ImagePicker.launchImageLibraryAsync({
-          mediaTypes: ImagePicker.MediaTypeOptions.Images,
-          allowsEditing: true,
-          //   aspect: [1, 1],
-          //   quality: 1,
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+            allowsEditing: true,
+            //   aspect: [1, 1],
+            //   quality: 1,
         });
-        
+
         if (!result.cancelled) {
-          set(result.uri)
-          console.log(result.uri)
-          // UploadImage(result.uri, setImageUrl)  // PRECISAMOS ARRUMAR ESSA GAMBIARRA!!!
+            set(result.uri)
+            console.log(result.uri)
+            // UploadImage(result.uri, setImageUrl)  // PRECISAMOS ARRUMAR ESSA GAMBIARRA!!!
         }
-      };
+    };
     const main = (set) => {
-        if(galleryPermission == true)
-            {
-              pickImage(set);
-            }else{
-              galeryPermisionFunction();
-            }
+        if (galleryPermission == true) {
+            pickImage(set);
+        } else {
+            galeryPermisionFunction();
+        }
     }
 
     useEffect(() => {
         galeryPermisionFunction();
         const getProfileData = async () => {
             await axios.get(`https://imovim-api.cyclic.app/profile/get-editingProfile-data/${id}`)
-            .then((res) => {
-                console.log(res.data)
-                res.data.forEach(element => {
-                    setImage(element.profileImage)
-                    setBackgroundImage(element.profileBackground)
-                    setLocalization(element.localization)
-                    setLoaded(true)
-                });
-            })
+                .then((res) => {
+                    console.log(res.data)
+                    res.data.forEach(element => {
+                        setImage(element.profileImage)
+                        setBackgroundImage(element.profileBackground)
+                        setLocalization(element.localization)
+                        setLoaded(true)
+                    });
+                })
         }
         getProfileData()
     }, [])
-    if(!loaded) {
-        return(
+    if (!loaded) {
+        return (
             <View>
                 <Text>Loading...</Text>
             </View>
@@ -97,25 +100,25 @@ function EditarPerfil({ navigation }) {
 
                 <TouchableOpacity onPress={() => main(setImage)} style={styles.editProfileIconContainerPhoto}>
                     {!image && (
-                    <View style={styles.editPhoto}>
-                        <Entypo name="camera" size={22} color="#ADADAD" />
-                    </View>
-                    ) }
-                    {image && (
-                        <Image 
-                       style =
-                       {
-                         {
-                             width: 160,
-                             height: 160,
-                             borderRadius: 100
-                         }
-                       }
-                       source={{uri: image}} 
-                       /> 
+                        <View style={styles.editPhoto}>
+                            <Entypo name="camera" size={22} color="#ADADAD" />
+                        </View>
                     )}
-                                    
-                    
+                    {image && (
+                        <Image
+                            style=
+                            {
+                                {
+                                    width: 160,
+                                    height: 160,
+                                    borderRadius: 100
+                                }
+                            }
+                            source={{ uri: image }}
+                        />
+                    )}
+
+
                 </TouchableOpacity>
 
                 <View style={styles.lineContainer}>
@@ -127,23 +130,23 @@ function EditarPerfil({ navigation }) {
                 </View>
 
                 <TouchableOpacity onPress={() => main(setBackgroundImage)} style={styles.editProfileIconContainerBanner}>
-                    { !backgroundImage && (
+                    {!backgroundImage && (
                         <View style={styles.editBanner}>
                             <Entypo name="camera" size={22} color="#ADADAD" />
                         </View>
-                    ) }
-                    { backgroundImage && (
-                        <Image 
-                        style =
-                        {
-                          {
-                              width: '95%',
-                              height: 150,
-                          }
-                        }
-                        source={{uri: backgroundImage}} 
-                        /> 
-                    ) }
+                    )}
+                    {backgroundImage && (
+                        <Image
+                            style=
+                            {
+                                {
+                                    width: '95%',
+                                    height: 150,
+                                }
+                            }
+                            source={{ uri: backgroundImage }}
+                        />
+                    )}
                 </TouchableOpacity>
 
                 <View style={styles.lineContainer}>
@@ -155,7 +158,7 @@ function EditarPerfil({ navigation }) {
                 </View>
 
                 <View style={styles.editProfileIconContainerInput}>
-                    <TextInput value={localization} onChangeText={(text) => {setLocalization(text)}} style={styles.input} />
+                    <TextInput value={localization} onChangeText={(text) => { setLocalization(text) }} style={styles.input} />
                 </View>
 
             </View>
