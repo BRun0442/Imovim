8ed5a84130b93c586e1ec3e7c9deb71737f96f56
Alunos <aluno.etec@etecEmbu.com.br>
2023-03-22 22:30:19 -0300

    showing uses posts

--- components/Pages/PerfilVisãoExterna/Perfil.js
+++ components/Pages/PerfilVisãoExterna/Perfil.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState, useContext } from 'react';
 import { View, Text, Image, StatusBar, Alert, TouchableOpacity } from 'react-native';
 import { styles } from './style'
+import { FlatList } from "react-native-gesture-handler";
+import Post from '../../Post/Post';
 
 import { ScrollView } from 'react-native-gesture-handler';
 import { Foundation } from '@expo/vector-icons';
@@ -20,6 +22,7 @@ export default function PerfilVisãoExterna({ navigation }, props) {
     const [name, setName] = useState('')
     const [location, setLocation] = useState('s')
     const [currentUser, setCurrentUser] = useState()
+    const [posts, setPosts] = useState()
     
     const getUserData = async () => {
         const data = await getAnotherUserData(anotherUser_id)
@@ -29,6 +32,7 @@ export default function PerfilVisãoExterna({ navigation }, props) {
         setLocation(data.profileInfo[0].localization)
         setName(data.profileInfo[0].nickname)
         setCurrentUser(data.profileInfo[0].user_id)
+        setPosts(data.userPosts)
         return data
     } 
 
@@ -44,61 +48,79 @@ export default function PerfilVisãoExterna({ navigation }, props) {
         )
     }
     return (
-        <ScrollView style={styles.container}>
-            <Header navigation={navigation} />
-
-            <View style={styles.background} />
-
-            <View style={styles.perfil}>
-
-                <View style={styles.iconsContainer}>
-                    {!profileImage && <View style={styles.iconCam}>
-                        <Entypo name="camera" size={22} color="white" />
-                    </View>}
-
-                    {profileImage && <Image style={styles.profileImage} source={{uri: profileImage}} />}
-
-                    <View style={styles.addFriendContainer}>
-                        <View>
-                            <TouchableOpacity
-                                style={styles.followButton}
-                                onPress={() => setChangeIcon(!changeIcon)}
-                            >
-                                {
-                                    changeIcon ?
-                                        <View style={styles.addFriendsIcons}>
-                                            <FontAwesome5 name="user-check" size={24} color="#FFF" />
-                                            <Text style={styles.addFriendText}>Seguindo</Text>
-                                        </View>
-                                        :
-                                        <View style={styles.addFriendsIcons} >
-                                            <Ionicons name="person-add" size={24} color="#FFF" />
-                                            <Text style={styles.addFriendText}>Seguir</Text>
-                                        </View>
-
-                                }
-                            </TouchableOpacity>
-                        </View>
-
-                        <View style={styles.iconsActions}>
-                            <Ionicons name="alert-circle" size={28} color="#F8670E" />
-                            <Entypo name="block" size={24} color="#F8670E" style={{ marginLeft: 8, marginRight: 8 }} />
-                            <Ionicons name="chatbubble" size={25} color="#F8670E" />
+        <FlatList style={styles.container}
+        data={posts}
+        renderItem={({ item }) => (
+            <Post
+              goToCommentScreen={() => {
+                setPostFocusedId(item.id);
+                navigation.navigate("Comentarios");
+              }}
+              likePost={async () => {
+                await likePost(accountData.profileInfo[0].id, item.id);
+                getUserData(accountData.profileInfo[0].id, setAccountData);
+              }}
+              {...item}
+            />
+          )} 
+        ListHeaderComponent={
+            <View>
+                <Header navigation={navigation} />
+        
+                <View style={styles.background} />
+        
+                <View style={styles.perfil}>
+        
+                    <View style={styles.iconsContainer}>
+                        {!profileImage && <View style={styles.iconCam}>
+                            <Entypo name="camera" size={22} color="white" />
+                        </View>}
+        
+                        {profileImage && <Image style={styles.profileImage} source={{uri: profileImage}} />}
+        
+                        <View style={styles.addFriendContainer}>
+                            <View>
+                                <TouchableOpacity
+                                    style={styles.followButton}
+                                    onPress={() => setChangeIcon(!changeIcon)}
+                                >
+                                    {
+                                        changeIcon ?
+                                            <View style={styles.addFriendsIcons}>
+                                                <FontAwesome5 name="user-check" size={24} color="#FFF" />
+                                                <Text style={styles.addFriendText}>Seguindo</Text>
+                                            </View>
+                                            :
+                                            <View style={styles.addFriendsIcons} >
+                                                <Ionicons name="person-add" size={24} color="#FFF" />
+                                                <Text style={styles.addFriendText}>Seguir</Text>
+                                            </View>
+        
+                                    }
+                                </TouchableOpacity>
+                            </View>
+        
+                            <View style={styles.iconsActions}>
+                                <Ionicons name="alert-circle" size={28} color="#F8670E" />
+                                <Entypo name="block" size={24} color="#F8670E" style={{ marginLeft: 8, marginRight: 8 }} />
+                                <Ionicons name="chatbubble" size={25} color="#F8670E" />
+                            </View>
                         </View>
                     </View>
-                </View>
-
-                <View style={styles.infos}>
-
-                    <View style={styles.data}>
-                        <Text style={styles.name}>{name}</Text>
-                        <Text style={styles.location}>{location}</Text>
+        
+                    <View style={styles.infos}>
+        
+                        <View style={styles.data}>
+                            <Text style={styles.name}>{name}</Text>
+                            <Text style={styles.location}>{location}</Text>
+                        </View>
+        
                     </View>
-
                 </View>
-            </View>
-            <View style={styles.border}></View>
+                <View style={styles.border}></View>
+                </View>
+            }
+        />
 
-        </ScrollView>
     );
 }
