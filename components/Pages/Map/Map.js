ac9e9a8d554d546b42ee73a0025195cef2851df3
Tiaguh <tiagosingup@gmail.com>
2023-05-15 10:53:49 -0300

    geting user position in map

--- components/Pages/Map/Map.js
+++ components/Pages/Map/Map.js
@@ -1,16 +1,49 @@
-import React from 'react';
-import MapView from 'react-native-maps';
-import { StyleSheet, View } from 'react-native';
-import Header from '../../Header/Header';
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
 
-export default function Map({navigation}) {
+import MapView from "react-native-maps";
+
+import * as Location from "expo-location";
+
+import Header from "../../Header/Header";
+
+export default function Map() {
+  const [location, setLocation] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } =
+        await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        console.log("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  console.log(location);
+
   return (
     <View style={styles.container}>
-      <Header navigation={navigation} />
-      <MapView style={styles.map} />
+      <Header />
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: 37.42597730214824,
+          longitude: -122.0856026405,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+        showsUserLocation={true}
+        loadingEnabled
+        mapType="mutedStandard"
+      />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
