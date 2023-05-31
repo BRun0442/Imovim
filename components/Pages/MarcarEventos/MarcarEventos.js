abd85d4179a8eb0f9cd549a17f978cbb10aec658
luisfelipe2104 <100540998+luisfelipe2104@users.noreply.github.com>
2023-05-31 14:08:33 -0300

    updating events

--- components/Pages/MarcarEventos/MarcarEventos.js
+++ components/Pages/MarcarEventos/MarcarEventos.js
@@ -1,6 +1,16 @@
 import React, { useState, useContext, useEffect } from "react";
-import { View, Text, TextInput, StatusBar, ScrollView, TouchableOpacity, SafeAreaView, KeyboardAvoidingView, Image } from "react-native";
-import { styles } from "./style"
+import {
+  View,
+  Text,
+  TextInput,
+  StatusBar,
+  ScrollView,
+  TouchableOpacity,
+  SafeAreaView,
+  KeyboardAvoidingView,
+  Image,
+} from "react-native";
+import { styles } from "./style";
 
 import Header from "../../Header/Header";
 import ProfileImage from "../../ProfileImage/ProfileImage";
@@ -12,239 +22,267 @@ import * as ImagePicker from "expo-image-picker";
 import * as MediaLibrary from "expo-media-library";
 
 import { AuthContext } from "../../../contexts/auth";
-import { createEvent, getEvent } from "../../../services/events";
-
-
+import { createEvent, getEvent, updateEvent } from "../../../services/events";
 
 export default function MarcarEventos({ navigation }, props) {
-    const { id, event_id, updatingEvent, setUpdatingEvent, marker, setAlterMapPermission } = useContext(AuthContext);
-    const { profilePicture, username } = useContext(AuthContext);
+  const {
+    id,
+    event_id,
+    updatingEvent,
+    setUpdatingEvent,
+    marker,
+    setAlterMapPermission,
+  } = useContext(AuthContext);
+  const { profilePicture, username } = useContext(AuthContext);
 
-    const [name, setName] = useState()
-    const [localization, setLocalization] = useState()
-    const [description, setDescription] = useState()
-    const [image, setImage] = useState(null)
-    const [day, setDay] = useState()
-    const [month, setMonth] = useState()
-    const [year, setYear] = useState()
-    const [hour, setHour] = useState()
-    const [minute, setMinute] = useState()
-    const [descriptionLength, setDescriptionLength] = useState(0)
+  const [name, setName] = useState("");
+  const [localization, setLocalization] = useState();
+  const [description, setDescription] = useState("");
+  const [image, setImage] = useState(null);
+  const [day, setDay] = useState();
+  const [month, setMonth] = useState();
+  const [year, setYear] = useState();
+  const [hour, setHour] = useState();
+  const [minute, setMinute] = useState();
+  const [descriptionLength, setDescriptionLength] = useState(0);
+  const [eventId, setEventId] = useState(null);
 
-    const [galleryPermission, setGalleryPermission] = useState(null);
+  const [galleryPermission, setGalleryPermission] = useState(null);
 
-    useEffect(() => {
-        // CameraPermisionFunction();
-        // setImage(null);
-        galeryPermisionFunction();
-    }, []);
+  useEffect(() => {
+    // CameraPermisionFunction();
+    // setImage(null);
+    galeryPermisionFunction();
+  }, []);
 
-    const galeryPermisionFunction = async () => {
-        const galleryPermissions = await MediaLibrary.requestPermissionsAsync();
-        setGalleryPermission(galleryPermissions.granted);
-    };
+  const galeryPermisionFunction = async () => {
+    const galleryPermissions = await MediaLibrary.requestPermissionsAsync();
+    setGalleryPermission(galleryPermissions.granted);
+  };
 
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      //   aspect: [1, 1],
+      //   quality: 1,
+    });
 
-    const pickImage = async () => {
-        // No permissions request is necessary for launching the image library
-        let result = await ImagePicker.launchImageLibraryAsync({
-            mediaTypes: ImagePicker.MediaTypeOptions.Images,
-            allowsEditing: true,
-            //   aspect: [1, 1],
-            //   quality: 1,
-        });
-
-        if (!result.cancelled) {
-            setImage(result.uri);
-            // UploadImage(result.uri, setImageUrl)  // PRECISAMOS ARRUMAR ESSA GAMBIARRA!!!
-        }
-    };
-
-    const main = async () => {
-        if (galleryPermission == true) {
-            pickImage();
-        } else {
-            galeryPermisionFunction();
-        }
+    if (!result.cancelled) {
+      setImage(result.uri);
+      // UploadImage(result.uri, setImageUrl)  // PRECISAMOS ARRUMAR ESSA GAMBIARRA!!!
     }
-    const handleSubmit = async () => {
-        console.log(marker);
-        if (!image) {
-            alert("Insira uma imagem no evento!")
-        } else {
-            console.log(id, name, localization, description, day, month, year, hour, minute)
-            const eventDate = `${year}/${month}/${day}`
-            const eventHour = `${hour}:${minute}`
-            await createEvent(id, name, eventDate, eventHour, localization, description, image, setImage, marker)
-        }
+  };
+
+  const main = async () => {
+    if (galleryPermission == true) {
+      pickImage();
+    } else {
+      galeryPermisionFunction();
     }
-
-    const getEspecificEvent = async () => {
-        await getEvent(id, event_id)
-            .then((event) => {
-                setName(event[0].event_name)
-                setImage(event[0].photo)
-                // setDate(event[0].event_date)
-                setHour(event[0].event_hour)
-                setLocalization(event[0].localization)
-                setDescription(event[0].description)
-                // setLatitude(event[0].latitude)
-                // setLongitude(event[0].longitude)
-            })
+  };
+  const handleSubmit = async () => {
+    console.log(marker);
+    if (!image) {
+      alert("Insira uma imagem no evento!");
+    } else {
+      console.log(
+        id,
+        name,
+        localization,
+        description,
+        day,
+        month,
+        year,
+        hour,
+        minute
+      );
+      const eventDate = `${year}/${month}/${day}`;
+      const eventHour = `${hour}:${minute}`;
+      await createEvent(
+        id,
+        name,
+        eventDate,
+        eventHour,
+        localization,
+        description,
+        image,
+        setImage,
+        marker,
+        updatingEvent,
+        eventId
+      );
     }
+  };
 
-    useEffect(() => {
-        if (updatingEvent) {
-            getEspecificEvent()
-            console.log('okkkk');
-        }
-    }, [event_id])
+  const getEspecificEvent = async () => {
+    await getEvent(id, event_id).then((event) => {
+      console.log(event[0]);
+      setName(event[0].event_name);
+      setEventId(event[0].id);
+      setImage(event[0].photo);
+      // setDate(event[0].event_date)
+      setHour(event[0].event_hour);
+      setLocalization(event[0].localization);
+      setDescription(event[0].description);
+      // setLatitude(event[0].latitude)
+      // setLongitude(event[0].longitude)
+    });
+  };
 
-    return (
-        <ScrollView style={styles.container}>
+  useEffect(() => {
+    if (updatingEvent) {
+      getEspecificEvent();
+      console.log("okkkk");
+    }
+  }, [event_id]);
 
-            <Header navigation={navigation} />
+  return (
+    <ScrollView style={styles.container}>
+      <Header navigation={navigation} />
 
-            <View style={styles.iconContainer}>
-                <MaterialCommunityIcons name="calendar" color={"#FFF"} size={26} />
+      <View style={styles.iconContainer}>
+        <MaterialCommunityIcons name="calendar" color={"#FFF"} size={26} />
+      </View>
+
+      <View style={styles.postContainer}>
+        <ProfileImage profileImage={profilePicture} />
+        <Text style={styles.profileName}>{username}</Text>
+      </View>
+
+      <View style={styles.form}>
+        <KeyboardAvoidingView behavior={"padding"}>
+          <Text style={styles.formText}>Nome do Evento</Text>
+          <View style={styles.nameInputs}>
+            <TextInput
+              style={styles.inputType1}
+              value={name}
+              onChangeText={(text) => setName(text)}
+              keyboardType="default"
+            />
+          </View>
+
+          <Text style={styles.formText}>Data</Text>
+          <View style={styles.dataInputs}>
+            <TextInput
+              style={styles.inputType2}
+              onChangeText={(text) => setDay(text)}
+              placeholder="Dia"
+              keyboardType="numeric"
+            />
+
+            <TextInput
+              style={styles.inputType2}
+              onChangeText={(text) => setMonth(text)}
+              placeholder="Mês"
+              keyboardType="numeric"
+            />
+
+            <TextInput
+              style={styles.inputType2}
+              onChangeText={(text) => setYear(text)}
+              placeholder="Ano"
+              keyboardType="numeric"
+            />
+          </View>
+
+          <Text style={styles.formText}>Hora</Text>
+
+          <View style={styles.horaInputs}>
+            <TextInput
+              style={styles.inputType2}
+              onChangeText={(text) => setHour(text)}
+              placeholder="hh"
+              keyboardType="numeric"
+            />
+
+            <TextInput
+              style={styles.inputType2}
+              onChangeText={(text) => setMinute(text)}
+              placeholder="min"
+              keyboardType="numeric"
+            />
+
+            <View style={styles.invisbleInput} />
+          </View>
+
+          <Text style={styles.formText}>Local</Text>
+          <View style={styles.borderLocationInput}>
+            <View style={styles.locationInput}>
+              <TextInput
+                style={styles.inputType4}
+                onChangeText={(text) => setLocalization(text)}
+                keyboardType="default"
+              />
+              <TouchableOpacity
+                style={styles.iconInput}
+                onPress={() => {
+                  setAlterMapPermission(true);
+                  navigation.navigate("Mapa");
+                }}
+              >
+                <Entypo name="location" size={22} color="#F8670E" />
+              </TouchableOpacity>
             </View>
+          </View>
 
-            <View style={styles.postContainer}>
-                <ProfileImage profileImage={profilePicture} />
-                <Text style={styles.profileName}>{username}</Text>
+          <Text style={styles.formText}>Descrição</Text>
+          <View style={styles.nameInputs}>
+            <TextInput
+              style={styles.inputType3}
+              maxLength={200}
+              value={description}
+              onChangeText={(text) => {
+                setDescription(text);
+                setDescriptionLength(text.length);
+              }}
+              keyboardType="default"
+              multiline={true}
+            />
+
+            <Text style={styles.descriptionLengthText}>
+              {descriptionLength} / 200
+            </Text>
+          </View>
+
+          <View style={styles.bannerContainer}>
+            <Text style={styles.formText}>Foto</Text>
+
+            <View style={styles.banner}>
+              <TouchableOpacity
+                onPress={() => pickImage()}
+                style={styles.editProfileIconContainerBanner}
+              >
+                {!image && (
+                  <View style={styles.editBanner}>
+                    <Entypo name="camera" size={22} color="#FFF" />
+                  </View>
+                )}
+
+                {image && (
+                  <Image
+                    style={{
+                      width: "95%",
+                      height: 150,
+                    }}
+                    source={{ uri: image }}
+                  />
+                )}
+              </TouchableOpacity>
             </View>
+          </View>
 
-            <View style={styles.form}>
-                <KeyboardAvoidingView behavior={"padding"} >
+          <TouchableOpacity
+            onPress={() => handleSubmit()}
+            style={styles.button}
+          >
+            <Text style={styles.text}>Criar Evento</Text>
+          </TouchableOpacity>
+        </KeyboardAvoidingView>
+      </View>
 
-                    <Text style={styles.formText}>Nome do Evento</Text>
-                    <View style={styles.nameInputs}>
-                        <TextInput style={styles.inputType1}
-                            onChangeText={(text) => setName(text)}
-                            keyboardType="default"
-                        />
-                    </View>
-
-                    <Text style={styles.formText}>Data</Text>
-                    <View style={styles.dataInputs}>
-
-                        <TextInput style={styles.inputType2}
-                            onChangeText={(text) => setDay(text)}
-                            placeholder="Dia"
-                            keyboardType="numeric"
-                        />
-
-                        <TextInput style={styles.inputType2}
-                            onChangeText={(text) => setMonth(text)}
-                            placeholder="Mês"
-                            keyboardType="numeric"
-                        />
-
-                        <TextInput style={styles.inputType2}
-                            onChangeText={(text) => setYear(text)}
-                            placeholder="Ano"
-                            keyboardType="numeric"
-                        />
-                    </View>
-
-                    <Text style={styles.formText}>Hora</Text>
-
-                    <View style={styles.horaInputs}>
-                        <TextInput style={styles.inputType2}
-                            onChangeText={(text) => setHour(text)}
-                            placeholder="hh"
-                            keyboardType="numeric"
-                        />
-
-                        <TextInput style={styles.inputType2}
-                            onChangeText={(text) => setMinute(text)}
-                            placeholder="min"
-                            keyboardType="numeric"
-                        />
-
-                        <View style={styles.invisbleInput} />
-
-                    </View>
-
-                    <Text style={styles.formText}>Local</Text>
-                    <View style={styles.borderLocationInput}>
-                        <View style={styles.locationInput}>
-                            <TextInput style={styles.inputType4}
-                                onChangeText={(text) => setLocalization(text)}
-                                keyboardType="default"
-                            />
-                            <TouchableOpacity
-                                style={styles.iconInput}
-                                onPress={() => {
-                                    setAlterMapPermission(true)
-                                    navigation.navigate("Mapa")
-                                }}
-                            >
-                                <Entypo name="location" size={22} color="#F8670E" />
-                            </TouchableOpacity>
-                        </View>
-                    </View>
-
-                    <Text style={styles.formText}>Descrição</Text>
-                    <View style={styles.nameInputs}>
-                        <TextInput style={styles.inputType3}
-                            maxLength={200}
-                            onChangeText={(text) => {
-                                setDescription(text)
-                                setDescriptionLength(text.length)
-                            }}
-                            keyboardType="default"
-                            multiline={true}
-                        />
-
-                        <Text style={styles.descriptionLengthText}>{descriptionLength} / 200</Text>
-                    </View>
-
-                    <View style={styles.bannerContainer}>
-
-                        <Text style={styles.formText}>Foto</Text>
-
-                        <View style={styles.banner}>
-                            <TouchableOpacity
-
-                                onPress={() => pickImage()}
-
-                                style={styles.editProfileIconContainerBanner}>
-
-                                {!image && (
-
-                                    <View style={styles.editBanner}>
-                                        <Entypo name="camera" size={22} color="#FFF" />
-                                    </View>
-
-                                )}
-
-                                {image && (
-                                    <Image
-                                        style=
-                                        {
-                                            {
-                                                width: "95%",
-                                                height: 150,
-                                            }
-                                        }
-                                        source={{ uri: image }}
-                                    />
-                                )}
-                            </TouchableOpacity>
-                        </View>
-
-                    </View>
-
-                    <TouchableOpacity onPress={() => handleSubmit()} style={styles.button}>
-                        <Text style={styles.text}>Criar Evento</Text>
-                    </TouchableOpacity>
-
-                </KeyboardAvoidingView>
-
-            </View>
-
-            <StatusBar />
-        </ScrollView >
-    );
+      <StatusBar />
+    </ScrollView>
+  );
 }
--- services/events.js
+++ services/events.js
@@ -1,11 +1,22 @@
-import api from './api';
+import api from "./api";
 import { firebaseConfig, storage } from "../firebase/config.js";
 import { uploadBytes, getDownloadURL, ref, getStorage } from "firebase/storage";
 import { initializeApp } from "firebase/app";
 
-export async function createEvent(user_id, event_name, event_date, event_hour, localization, description, image, setImage, marker) {
-  if(image)
-  {
+export async function createEvent(
+  user_id,
+  event_name,
+  event_date,
+  event_hour,
+  localization,
+  description,
+  image,
+  setImage,
+  marker,
+  updatingEvent,
+  eventId
+) {
+  if (image) {
     let imageRef;
     const storageUrl = `eventsImage/${Date.now()}/${Math.random().toString()}`;
     const response = await fetch(image);
@@ -13,107 +24,190 @@ export async function createEvent(user_id, event_name, event_date, event_hour, l
     imageRef = ref(storage, storageUrl);
 
     uploadBytes(imageRef, bytes)
-    .then(() => {
-      getDownloadURL(imageRef)
-        .then(async (url) => {
-          await sendDataToApi(user_id, event_name, event_date, event_hour, localization, description, url, marker)  
-          .then(res => console.log(res))
-          console.log('url:', url)
-          url = ''
-          setImage(null)
-          return url
-        })
-        .catch((err) => {
-          console.log(err.message);
-        });
-    })
-    .catch((err) => {
-      console.log(err.message);
-    });
-  }else{
+      .then(() => {
+        getDownloadURL(imageRef)
+          .then(async (url) => {
+            if (!updatingEvent) {
+              await sendDataToApi(
+                user_id,
+                event_name,
+                event_date,
+                event_hour,
+                localization,
+                description,
+                url,
+                marker
+              ).then((res) => console.log(res));
+              console.log("url:", url);
+              url = "";
+              setImage(null);
+              return url;  
+            } else {
+              await updateEvent(
+                user_id,
+                eventId,
+                event_name,
+                event_date,
+                event_hour,
+                localization,
+                description,
+                url,
+                marker
+              ).then((res) => console.log(res));
+              console.log("url:", url);
+              url = "";
+              setImage(null);
+              return url;  
+            }
+          })
+          .catch((err) => {
+            console.log(err.message);
+          });
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  } else {
     imageRef = null;
-    sendDataToApi(user_id, event_name, event_date, event_hour, localization, description, image, marker)
-    setImage(null)  
+    if (!updatingEvent) {
+      sendDataToApi(
+        user_id,
+        event_name,
+        event_date,
+        event_hour,
+        localization,
+        description,
+        image,
+        marker
+      )
+    } else {
+      updateEvent(
+        user_id,
+        eventId,
+        event_name,
+        event_date,
+        event_hour,
+        localization,
+        description,
+        url,
+        marker
+      )
+    }
+    setImage(null);
     return;
   }
 }
 
-export async function sendDataToApi(user_id, event_name, event_date, event_hour, localization, description, photo, marker)
-{
+export async function sendDataToApi(
+  user_id,
+  event_name,
+  event_date,
+  event_hour,
+  localization,
+  description,
+  photo,
+  marker
+) {
   try {
     const response = await api.post("/event/create-event", {
-      user_id: user_id, 
-      event_name: event_name, 
-      event_date: event_date, 
-      event_hour: event_hour, 
-      localization: localization, 
-      description: description, 
+      user_id: user_id,
+      event_name: event_name,
+      event_date: event_date,
+      event_hour: event_hour,
+      localization: localization,
+      description: description,
       photo: photo,
-      marker: marker
-    })
-    
-    alert(response.data.msg)
-    console.log(response)
+      marker: marker,
+    });
+
+    alert(response.data.msg);
+    console.log(response);
   } catch (error) {
-    console.log(error.data.msg)
+    console.log(error.data.msg);
   }
 }
 
-export async function getAllEvents(user_id)
-{
+export async function getAllEvents(user_id) {
   try {
-    const response = await api.get(`/event/get-all-events/${user_id}`)
-    return response.data
+    const response = await api.get(`/event/get-all-events/${user_id}`);
+    return response.data;
   } catch (error) {
-    console.log(error.data.msg)
+    console.log(error.data.msg);
   }
 }
 
-export async function getEvent(user_id, event_id)
-{
+export async function getEvent(user_id, event_id) {
   try {
-    const response = await api.get(`/event/get-event/${user_id}/${event_id}`)
-    return response.data
+    const response = await api.get(`/event/get-event/${user_id}/${event_id}`);
+    return response.data;
   } catch (error) {
-    console.log(error.data.msg)
+    console.log(error.data.msg);
   }
 }
 
-export async function getMyEvents(user_id)
-{
+export async function updateEvent(
+  user_id,
+  event_id,
+  event_name,
+  event_date,
+  event_hour,
+  localization,
+  description,
+  photo,
+  marker) {
   try {
-    const response = await api.get(`/event/get-user-events/${user_id}`)
-    return response.data
+    const response = await api.put("/event/update-event", {
+      user_id: user_id,
+      event_id: event_id,
+      event_name: event_name,
+      event_date: event_date,
+      event_hour: event_hour,
+      localization: localization,
+      description: description,
+      photo: photo,
+      marker: marker,
+    });
+
+    alert(response.data.msg);
+    console.log(response);
   } catch (error) {
-    console.log(error.data.msg)
+    console.log(error.data.msg);
   }
 }
 
-export async function getSavedEvents(user_id)
-{
+export async function getMyEvents(user_id) {
   try {
-    const response = await api.get(`/event/get-saved-events/${user_id}`)
-    return response.data
+    const response = await api.get(`/event/get-user-events/${user_id}`);
+    return response.data;
   } catch (error) {
-    console.log(error.data.msg)
+    console.log(error.data.msg);
+  }
+}
+
+export async function getSavedEvents(user_id) {
+  try {
+    const response = await api.get(`/event/get-saved-events/${user_id}`);
+    return response.data;
+  } catch (error) {
+    console.log(error.data.msg);
   }
 }
 
 export async function goToEvent(user_id, event_id) {
-  const data = {user_id, event_id}
+  const data = { user_id, event_id };
   try {
-    await api.post(`/event/go-to-event/`, data)
+    await api.post(`/event/go-to-event/`, data);
   } catch (error) {
-    console.log(error.data.msg)
+    console.log(error.data.msg);
   }
 }
 
 export async function saveEvent(user_id, event_id) {
-  const data = {user_id, event_id}
+  const data = { user_id, event_id };
   try {
-    await api.post(`/event/save-event/`, data)
+    await api.post(`/event/save-event/`, data);
   } catch (error) {
-    console.log(error.data.msg)
+    console.log(error.data.msg);
   }
 }
 
